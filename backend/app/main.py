1c56705a0c4eaef881d542e19402dc51f9dd9c1b
abusufiyan7518 <abusufiyantechsak@example.com>
2026-07-04 17:12:51 +0530

    chore: prepare backend for deployment

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .database import engine
 from .models import Base
 from .routes import auth_routes, document_routes, chat_routes
-
-from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
 
@@ -14,12 +13,14 @@ app = FastAPI(
     description="Backend API for RAG-Based Enterprise Knowledge Assistant"
 )
 
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-    ],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
